8676fc89f3350e72359e67f8896cb1ad598b80a4
Zikrydng <bangzikrydng@gmail.com>
2024-06-19 12:44:00 +0800

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -69,10 +69,6 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
-        # 'OPTIONS': {
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        #     'charset': 'utf8mb4',
-        # }
     }
 }
 
